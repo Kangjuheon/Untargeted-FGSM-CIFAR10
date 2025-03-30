d8de02f51e4dfb877ea8e51a937a3e10bf56b44b
kangjuheon <2715wngjs@naver.com>
2025-03-30 22:34:29 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms, models
+from torchvision import datasets, transforms
+from torchvision.models import resnet18
 from torch.utils.data import DataLoader
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
